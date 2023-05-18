56598f38fc6d4f39f92191f5cd5daedff5512eab
Xyrylle Belmes <xyryllejoyb@gmail.com>
2023-05-18 21:15:25 +0800

    Added Destroy Functionality in CarouselItemsController

--- app/Http/Controllers/Api/CarouselItemsController.php
+++ app/Http/Controllers/Api/CarouselItemsController.php
@@ -47,6 +47,10 @@ class CarouselItemsController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $carouselItem = CarouselItems::findOrfail($id);
+ 
+        $carouselItem->delete();
+
+        return $carouselItem;
     }
 }
